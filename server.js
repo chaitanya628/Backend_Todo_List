dd8467d84121369bb53663b2019059075e4a1a64
chait164 <chaitanya.chandwani@techconfer.in>
2023-07-29 19:32:03 +0530

    update

--- server.js
+++ server.js
@@ -8,12 +8,12 @@ const DB_URL = process.env.DB_URL;
 
 // Middleware
 app.use(express.json());
-const corsOptions = {
-  origin: "*",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
+// const corsOptions = {
+//   origin: "*",
+//   credentials: true, //access-control-allow-credentials:true
+//   optionSuccessStatus: 200,
+// };
+app.use(cors());
 
 //connect with DB
 mongoose.set("strictQuery", true);
